3706b4293912125cfbc8d1724b2dca95c88d29ce
joy <13521131@std.stei.itb.ac.id>
2025-06-13 21:29:42 +0700

    just minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-// Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-// example/lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:ultralytics_yolo_example/presentation/screens/camera_inference_screen.dart';
 
